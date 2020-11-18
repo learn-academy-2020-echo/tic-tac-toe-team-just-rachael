aea8468983c95a0a105063234416345c2419bd46
Sarah <42073869+sjproctor@users.noreply.github.com>
2020-11-18 14:37:11 -0800

    Initial commit

---
+++ src/components/Square.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class Square extends Component{
+  render(){
+    return(
+      <React.Fragment>
+        <div id="square"></div>
+      </React.Fragment>
+    )
+  }
+}
+export default Square
